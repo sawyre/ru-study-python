64d65a1d8955edfbd47819700df8c09337065b5d
vladislav.vlasenko <vladislav.vlasenko@sparm.com>
2023-05-12 13:09:40 +0300

    feat: Add first task

--- maps/maps.py
+++ maps/maps.py
@@ -1,7 +1,32 @@
 from typing import Union
+from functools import partial
 
 
 class MapExercise:
+    @staticmethod
+    def has_required_elems_number(
+        row: dict, field: str, required_elem_number: int, sep: str = ','
+    ) -> bool:
+        """
+        Проверка на наличие указанного количество элементов в строке.
+
+        :param row: Фильтруемый объект
+        :param field: Фильтруемое поле
+        :param required_elem_number: Требуемое количество объектов в поле
+        :param sep: Разделитель объектов
+        :return: возвращает True в случае, если количество объектов разделенных через sep
+        больше или равно required_elem_number
+        """
+        elem_number = 0
+        if row[field] != "":
+            for symbol in row[field]:
+                if symbol == sep:
+                    elem_number += 1
+            elem_number += 1
+        return elem_number >= required_elem_number
+
+        
+
     @staticmethod
     def rating(list_of_movies: list[dict]) -> float:
         """
@@ -13,7 +38,22 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+        filtered_movies = list_of_movies[
+            map(
+                partial(has_required_elems_number, field="country", required_elem_number = 2), 
+                list_of_movies
+            )
+        ]
+        rating_of_filtered_movies = list(
+            map(
+                lambda row: int(row["rating_kinopoisk"]) if row["rating_kinopoisk"] else 0,
+                list_of_movies
+            )
+        )
+        rating_of_filtered_movies = rating_of_filtered_movies[
+            rating_of_filtered_movies > 0
+        ]
+        return sum(rating_kinopoisk) / len(rating_kinopoisk)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
