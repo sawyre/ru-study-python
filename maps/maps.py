48d244422a1818ad175f8478171f2269d8fa6b3f
vladislav.vlasenko <vladislav.vlasenko@sparm.com>
2023-05-12 16:04:09 +0300

    Add second task

--- maps/maps.py
+++ maps/maps.py
@@ -19,10 +19,11 @@ class MapExercise:
         """
         elem_number = 0
         if row[field] != "":
-            for symbol in row[field]:
-                if symbol == sep:
-                    elem_number += 1
-            elem_number += 1
+            elem_number = reduce(
+                lambda elem_number, symbol: elem_number + 1 if symbol == sep,
+                row[field],
+                1
+            )
         return elem_number >= required_elem_number
 
         
@@ -38,22 +39,34 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        filtered_movies = list_of_movies[
-            map(
-                partial(has_required_elems_number, field="country", required_elem_number = 2), 
-                list_of_movies
-            )
-        ]
+        filtered_movies = filter(
+            partial(has_required_elems_number, field="country", required_elem_number = 2), 
+            list_of_movies
+        )
         rating_of_filtered_movies = list(
             map(
-                lambda row: int(row["rating_kinopoisk"]) if row["rating_kinopoisk"] else 0,
+                lambda row: float(row["rating_kinopoisk"]) if row["rating_kinopoisk"] else 0,
                 list_of_movies
             )
         )
-        rating_of_filtered_movies = rating_of_filtered_movies[
-            rating_of_filtered_movies > 0
-        ]
-        return sum(rating_kinopoisk) / len(rating_kinopoisk)
+        rating_of_filtered_movies = filter(
+            lambda rating: rating > 0,
+            rating_of_filtered_movies,
+        )
+        rating_sum = reduce(
+            lambda rating_sum, rating: rating_sum + rating, 
+            rating_of_filtered_movies
+        )
+        return rating_sum / len(rating_kinopoisk)
+
+    @staticmethod
+    def count_symbols(obj: str, counted_symbol: str):
+        symbols_count = reduce(
+            lambda symbols_number, symbol: symbols_number + 1 if lower(symbol) == counted_symbol,
+            obj,
+            0
+        )
+        return symbols_number
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -68,4 +81,15 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+
+        filtered_movies = filter(
+            lambda row: row["rating_kinopoisk"] and float(row["rating_kinopoisk"]) >= rating, 
+            list_of_movies
+        )
+        symbols_count = reduce(lambda sum_symbols, x: sum_symbols + x,
+            map(
+                partial(count_symbols, counted_symbol="и"),
+                filtered_movies
+            )
+        )
+        return symbols_count
